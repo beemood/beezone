2852e47ed6b2a3c8024d0676a1daedcf4856c2a2
Robert Brightline <robert.brightline@gmail.com>
2025-07-13 17:34:02 -0500

    (prisma) prisma orm projects

--- apps/sample/src/lib/sample.module.ts
+++ apps/sample/src/lib/sample.module.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @nx/enforce-module-boundaries */
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FindMany, ResourceController } from '@beezone/nest';
+import { PrismaClientModule } from '@beezone/prisma-client';
+import { PrismaClient } from '@beezone/sample-db';
 
 @ResourceController()
 export class SampleController {
@@ -10,7 +13,10 @@ export class SampleController {
   }
 }
 @Module({
-  imports: [ConfigModule.forRoot({})],
+  imports: [
+    ConfigModule.forRoot({}),
+    PrismaClientModule.forRoot({ clientClass: PrismaClient }),
+  ],
   controllers: [SampleController],
 })
 export class SampleModule {}
