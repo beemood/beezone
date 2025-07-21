26997b6ad3ba86a2ca07604f5fc459882cb1aed1
Robert Brightline <robert.brightline@gmail.com>
2025-07-21 10:56:08 -0500

    (executor) update-version executor

--- libs/cli/src/executors/update-version/update-version.ts
+++ libs/cli/src/executors/update-version/update-version.ts
@@ -1,13 +1,19 @@
-import type { PromiseExecutor } from '@nx/devkit';
-import type { UpdateVersionExecutorSchema } from './schema';
+import { readJsonFile, updateJsonFile } from '@beezone/fs';
+import { type PromiseExecutor } from '@nx/devkit';
+import { join } from 'path';
 
-const runExecutor: PromiseExecutor<UpdateVersionExecutorSchema> = async (
-  options,
-  context
-) => {
-  console.log(
-    `Updating versions to ${options.version} for the project ${context.projectName}`
+const runExecutor: PromiseExecutor = async (options, context) => {
+  const mainProjectJson = await readJsonFile<{ version: string }>(
+    'package.json'
   );
+  await updateJsonFile<{ version: string }>(
+    join(context.root, 'package.json'),
+    (value) => {
+      value.version = mainProjectJson.version;
+      return value;
+    }
+  );
+
   return {
     success: true,
   };
