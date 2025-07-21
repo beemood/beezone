6f689ee0f95ce8634830366eed888e15778527ac
Robert Brightline <robert.brightline@gmail.com>
2025-07-21 10:59:25 -0500

    (cli) created the update-version executor

--- libs/cli/src/index.ts
+++ libs/cli/src/index.ts
@@ -1,8 +1,1 @@
 // @index(['./**/*.ts', '!./**/*.{spec,test}.ts'], f => `export * from '${f.path}.js'`)
-export * from './executors/update-version/schema.d.js';
-export * from './executors/update-version/update-version.js';
-export * from './generators/project/project.js';
-export * from './generators/project/schema.d.js';
-export * as doc from './plugins/doc/plugin.js';
-export * as updateVersion from './plugins/update-version/plugin.js';
-
