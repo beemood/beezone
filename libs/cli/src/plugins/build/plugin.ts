d856489390df325ccbbdebd3e295b9c30b639e97
Robert Brightline <robert.brightline@gmail.com>
2025-07-21 13:49:26 -0500

    (executors) global build build-watch executors

---
+++ libs/cli/src/plugins/build/plugin.ts
@@ -0,0 +1,52 @@
+import type { CreateNodesV2 } from '@nx/devkit';
+import { createNodesFromFiles } from '@nx/devkit';
+import { dirname, join } from 'path';
+
+/**
+ * Build libs/apps
+ * @group plugin
+ */
+export const createNodesV2: CreateNodesV2 = [
+  '**/package.json',
+  async (configFiles, options, context) => {
+    return await createNodesFromFiles(
+      (configFile) => {
+        const root = join(dirname(configFile));
+        return {
+          projects: {
+            [root]: {
+              targets: {
+                build: {
+                  executor: '@nx/js:swc',
+                  outputs: ['{options.outputPath}'],
+                  options: {
+                    outputPath: '{projectRoot}/dist',
+                    main: '{projectRoot}/src/index.ts',
+                    tsConfig: '{projectRoot}/tsconfig.lib.json',
+                    skipTypeCheck: true,
+                    stripLeadingPaths: true,
+                  },
+                },
+                'build-watch': {
+                  executor: '@nx/js:swc',
+                  outputs: ['{options.outputPath}'],
+                  options: {
+                    outputPath: '{projectRoot}/dist',
+                    main: '{projectRoot}/src/index.ts',
+                    tsConfig: '{projectRoot}/tsconfig.lib.json',
+                    skipTypeCheck: true,
+                    stripLeadingPaths: true,
+                    watch: true,
+                  },
+                },
+              },
+            },
+          },
+        };
+      },
+      configFiles,
+      options,
+      context
+    );
+  },
+];
--- libs/cli/src/plugins/serve/plugin.ts
+++ libs/cli/src/plugins/serve/plugin.ts
@@ -7,27 +7,15 @@ import { dirname, join } from 'path';
  * @group plugin
  */
 export const createNodesV2: CreateNodesV2 = [
-  '**/src/main.ts',
+  'apps/*/package.json',
   async (configFiles, options, context) => {
     return await createNodesFromFiles(
       (configFile) => {
-        const root = join(dirname(configFile), '..');
+        const root = join(dirname(configFile));
         return {
           projects: {
             [root]: {
               targets: {
-                'build-watch': {
-                  executor: '@nx/js:swc',
-                  outputs: ['{options.outputPath}'],
-                  options: {
-                    outputPath: '{projectRoot}/dist',
-                    main: '{projectRoot}/src/index.ts',
-                    tsConfig: '{projectRoot}/tsconfig.lib.json',
-                    skipTypeCheck: true,
-                    stripLeadingPaths: true,
-                    watch: true,
-                  },
-                },
                 'serve-static': {
                   executor: 'nx:run-commands',
                   options: {
