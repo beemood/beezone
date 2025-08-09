16bd649784b644936ba54da6548e97902515f317
Robert Brightline <robert.brightline@gmail.com>
2025-08-09 15:17:29 -0500

    (json) created prisma schema generator

--- libs/json/src/index.ts
+++ libs/json/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/bundle/bundle-json-schema-7.js';
-export * from './lib/typify/typify-json-schema-7.js';
 export * from './lib/prisma/generate-prisma-schema.js';
+export * from './lib/typify/typify-json-schema-7.js';
+
